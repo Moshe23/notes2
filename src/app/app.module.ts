f2789d13f4a4b6ff16b45a713772b000cb07a0e3
Moshe A <moshe.azulay@gmail.com>
2018-08-04 23:59:47 +0300

    new material start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import { NoteListComponent } from './note-list/note-list.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { NoteEditComponent } from './note-edit/note-edit.component';
 
 const appRoutes: Routes = [
   { path: 'notes', component: NoteListComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'note/:id', component: NoteEditComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     NoteListComponent,
-    HomeComponent
+    HomeComponent,
+    NoteEditComponent
   ],
   imports: [
     BrowserModule,
